0b6bc2ab62854f143cae679696994c33c5bb9b89
rna-afk <anarayan@redhat.com>
2021-06-17 14:50:21 -0400

    azurerm: Added image and storage account sas support for ASH
    Added the azurerm_image and azurerm_storage_account_sas support for
    Azure Stack Hub.

--- azurestack/provider.go
+++ azurestack/provider.go
@@ -83,6 +83,7 @@ func Provider() terraform.ResourceProvider {
 			"azurestack_route_table":             dataSourceArmRouteTable(),
 			"azurestack_subnet":                  dataSourceArmSubnet(),
 			"azurestack_virtual_network_gateway": dataSourceArmVirtualNetworkGateway(),
+			"azurestack_storage_account_sas":     dataSourceStorageAccountSharedAccessSignature(),
 		},
 
 		ResourcesMap: map[string]*schema.Resource{
@@ -115,6 +116,7 @@ func Provider() terraform.ResourceProvider {
 			"azurestack_virtual_machine_extension":          resourceArmVirtualMachineExtensions(),
 			"azurestack_virtual_network_gateway_connection": resourceArmVirtualNetworkGatewayConnection(),
 			"azurestack_virtual_machine_scale_set":          resourceArmVirtualMachineScaleSet(),
+			"azurestack_image":                              resourceImage(),
 		},
 	}
 
